3cc06d04368f275b8e46a74f0a8ca0db422b0936
metoZZ <80513638+metoZZ@users.noreply.github.com>
2021-03-31 13:02:52 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,7 @@
-
+# time series analyss
+# Greenland increase of temperature
+# settiamo la cartella di lavoro
+# install.packages("raster")
+library(raster)
+# settiamo la cartella di lavoro
+setwd("~/lab/greenland") 
