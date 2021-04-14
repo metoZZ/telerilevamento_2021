44ecb6f84d1c7c4ae120e72de91fa0a10eaf1e1e
metoZZ <80513638+metoZZ@users.noreply.github.com>
2021-04-14 12:59:41 +0200

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,7 +1,13 @@
 # visualizing Copernicus
+# scarichiamo l'indice che ci interessa sul sito di Copernicus, io ho scaricato l'indice del lago
 
 # lezione 12 mer 14 apr
 library(raster)
 install.packages("ncdf4")
 library(ncdf4)
 setwd("~/lab/")
+lake <- raster("c_gls_LSWT_202103110000_GLOBE_SLSTRAB_v1.1.0.nc") # associamo il nome al file della variabile
+lake # vediamo le caratteristiche del file
+plot (lake)
+lakeres <- aggregate(lake,fact=100) # resampling, ricampioniamo la variabile perchè è molto grande, 100*100=1 pixel
+plot(lakeres)
