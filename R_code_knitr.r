ac8265eef926c5e4de1c786e57a8eccd5627e4be
metoZZ <80513638+metoZZ@users.noreply.github.com>
2021-04-18 18:01:24 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,5 +1,9 @@
 # R_code_knitr.r
 
+# lezione 13: ven 16 apr
+
 setwd("~/lab/") # come sempre impostiamo la cartella di lavoro
 
 library(knitr)
+
+stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr")) # funzione che richiamo uno script .r per poi creare un report
